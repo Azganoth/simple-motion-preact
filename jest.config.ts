0baf6dad6459caf977837ae7a38728b940112451
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2024-12-27 23:26:18 -0300

    chore: add Storybook

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,5 @@ import type { Config } from "jest";
 export default {
   preset: "jest-preset-preact",
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/", "\\.stories\\.[jt]sx?$"],
 } satisfies Config;
